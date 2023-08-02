9251f5e3d04be45cd8f1ddba6c6673af676dbc4f
aasthavermacapg <vaastha16@gmail.com>
2023-08-02 23:39:26 +0530

    21th Commit

---
+++ force-app/main/default/lwc/internationalization/internationalization.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+import LOCALE from '@salesforce/i18n/locale';
+import CURRENCY from '@salesforce/i18n/currency';
+import DIR from '@salesforce/i18n/dir';
+export default class Internationalization extends LightningElement {
+    dir = DIR;
+    number = 6773874.45;
+    formattedNumber = new Intl.NumberFormat(LOCALE,{
+        style:'currency',
+        currency:CURRENCY,
+        currencyDisplay:'symbol'
+    }).format(this.number)
+    	
+    formattedNumber1 = new Intl.NumberFormat('en-US',{
+        style:'currency',
+        currency:'USD',
+        currencyDisplay:'symbol'
+    }).format(this.number)
+
+    formattedNumber2 = new Intl.NumberFormat('ar-EG',{
+        style:'currency',
+        currency:'USD',
+        currencyDisplay:'symbol'
+    }).format(this.number)
+}
